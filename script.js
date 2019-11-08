8979021afde710f063d84fe8c002d73a893070bf
minecire <minecirecire@gmail.com>
2019-11-07 19:05:08 -0500

    more basikz

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-game = document.getElementById("game");
-ctx = game.getContext("2d");
-
-loop = setInterval(runFrame, 1000/60);
+var game = document.getElementById("game");
+var ctx = game.getContext("2d");
+var blockSize;
+var loop = setInterval(runFrame, 1000/60);
 
 function runFrame(){
     game.width = window.innerWidth;
     game.height = window.innerHeight;
+    blockSize = game.width / 50;
     ctx.fillStyle = "#003366";
     ctx.fillRect(0,0,game.width,game.height);
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(blockSize*12, game.height-20*blockSize, blockSize*10, blockSize*20);
 }
