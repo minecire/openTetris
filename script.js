0dae9972b9c05a09cded671ff7e061e5b42adbd3
minecire <minecirecire@gmail.com>
2019-11-08 07:26:49 -0500

    tetrimino basic beginning

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var game = document.getElementById("game");
 var ctx = game.getContext("2d");
 var blockSize;
 var loop = setInterval(runFrame, 1000/60);
+var tetriminos = [];
 
 function runFrame(){
     game.width = window.innerWidth;
@@ -11,4 +12,13 @@ function runFrame(){
     ctx.fillRect(0,0,game.width,game.height);
     ctx.fillStyle = "#000000";
     ctx.fillRect(blockSize*12, game.height-20*blockSize, blockSize*10, blockSize*20);
+}
+
+class tetrimino{
+    constructor(type){
+        this.shape = type;
+        this.state = 3;
+        this.x = 10;
+        this.y = 0;
+    }
 }
